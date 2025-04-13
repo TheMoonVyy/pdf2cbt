6ca88b92002dea87dbaafafa780047be924a200f
TheMoonVyy <TheMoonVyy@gmail.com>
2025-04-13 08:01:45 +0530

    feat(components): add CbtResultsAnswerKeyDialog, CbtResultsImportExportDialog, and CbtResultsOverviewCard components

---
+++ components/Cbt/Results/CbtResultsImportExportDialog.vue
@@ -0,0 +1,68 @@
+<template>
+  <Dialog
+    v-model:visible="visibility"
+    :header="dialogLabel"
+    :modal="true"
+    :closable="true"
+    :draggable="false"
+    maximizable
+    pt:root:class="mx-auto"
+    pt:title:class="p-0 mx-auto"
+    pt:content:class="p-0"
+    pt:header:class="gap-4"
+  >
+    <div class="flex mb-5">
+      <BaseButton
+        :label="dialogLabel"
+        class="mx-auto"
+        :disabled="!selectedKeys.size"
+        @click="processData()"
+      />
+    </div>
+    <div class="flex flex-row justify-center flex-wrap gap-6 py-4 px-2 sm:px-4 md:px-8">
+      <div
+        v-for="(testOutputData, index) in data"
+        :key="index"
+      >
+        <CbtResultsOverviewCard
+          :test-result-overview="testOutputData.testResultOverview!"
+          read-only
+          class="cursor-pointer select-none"
+          :selected="selectedKeys.has(index)"
+          @click="() => selectedKeys.has(index) ? selectedKeys.delete(index) : selectedKeys.add(index)"
+        />
+      </div>
+    </div>
+  </Dialog>
+</template>
+
+<script lang="ts" setup>
+import type { TestOutputData } from '~/src/types'
+
+const selectedKeys = ref(new Set<number>())
+
+const visibility = defineModel<boolean>('visibility', {
+  default: true,
+})
+
+const emit = defineEmits<{
+  processed: [type: string, data: TestOutputData[]]
+}>()
+
+const props = defineProps<{
+  type: string
+  data: TestOutputData[]
+}>()
+
+const processData = () => {
+  const processedData: TestOutputData[] = []
+
+  for (const i of selectedKeys.value.values()) {
+    processedData.push(props.data[i])
+  }
+
+  emit('processed', props.type, processedData)
+}
+
+const dialogLabel = utilKeyToLabel(props.type) + ' Test Data'
+</script>
