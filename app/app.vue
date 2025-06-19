94f65e9cddc238c4e72a03d8ae388d1703fc211a
TheMoonVyy <TheMoonVyy@gmail.com>
2025-06-19 14:16:58 +0530

    feat(settings): centralize site settings to per page basis, migrate old settings to current one
    Migrate localStorage settings to group by page instead of handling individual keys separately.
    Normalize all color values using utilEnsureHashInHexColor() to ensure a hash prefix as nuxt color picker component requires it.
    Upgrade Dexie schema to version 4 and normalize stored CBT Interface's UI Settings color fields during upgrade.

--- app/app.vue
+++ app/app.vue
@@ -11,9 +11,67 @@
 </template>
 
 <script setup lang="ts">
-import { MiscConsts } from '#shared/enums'
+import { MiscConsts, DeprecatedLocalStorageKeys } from '#shared/enums'
 
 const showBackupWebsiteNotice = shallowRef(false)
+
+function checkAndMigratePdfCropperSettings() {
+  const oldSettingsString = localStorage.getItem(DeprecatedLocalStorageKeys.PDfCropperOldSettings)
+  if (!oldSettingsString) return
+  localStorage.removeItem(DeprecatedLocalStorageKeys.PDfCropperOldSettings)
+
+  const oldSettings = JSON.parse(oldSettingsString) as Partial<PdfCropperSettings['general']>
+  if (!oldSettings) return
+
+  const settings = usePdfCropperLocalStorageSettings()
+  const generalSettings = utilCloneJson(settings.value.general)
+
+  utilSelectiveMergeObj(generalSettings, oldSettings)
+
+  const colorKeys = [
+    'pageBGColor',
+    'cropSelectedRegionColor',
+    'cropSelectionGuideColor',
+    'cropSelectionSkipColor',
+  ] as const
+
+  for (const key of colorKeys) {
+    if (key in generalSettings) {
+      const colorValue = generalSettings[key]
+      if (colorValue) {
+        generalSettings[key] = utilEnsureHashInHexColor(colorValue)
+      }
+    }
+  }
+  settings.value.general = generalSettings
+}
+
+function checkAndMigrateCbtResultsSettings() {
+  const imgBgColor = localStorage.getItem(DeprecatedLocalStorageKeys.ResultsQuestionPanelImgBgColor)
+  const drawerWidth = localStorage.getItem(DeprecatedLocalStorageKeys.ResultsQuestionPanelWidth)
+
+  if (!imgBgColor && !drawerWidth) return
+
+  localStorage.removeItem(DeprecatedLocalStorageKeys.ResultsQuestionPanelImgBgColor)
+  localStorage.removeItem(DeprecatedLocalStorageKeys.ResultsQuestionPanelWidth)
+
+  const quePreview: Partial<CbtResultsSettings['quePreview']> = {}
+
+  if (imgBgColor) {
+    quePreview.imgBgColor = utilEnsureHashInHexColor(imgBgColor)
+  }
+
+  if (drawerWidth) {
+    const drawerWidthInInt = parseInt(drawerWidth)
+    if (drawerWidthInInt) {
+      quePreview.drawerWidth = drawerWidthInInt
+    }
+  }
+
+  const settings = useCbtResultsLocalStorageSettings()
+
+  utilSelectiveMergeObj(settings.value.quePreview, quePreview)
+}
 
 onMounted(() => {
   const config = useRuntimeConfig()
@@ -22,5 +80,8 @@ onMounted(() => {
       showBackupWebsiteNotice.value = true
     }
   }
+
+  checkAndMigratePdfCropperSettings()
+  checkAndMigrateCbtResultsSettings()
 })
 </script>
