32988d996e2ad3b2e0386291bdba158a90ef1fc5
TheMoonVyy <TheMoonVyy@gmail.com>
2025-06-19 13:40:22 +0530

    feat(ui): add shadcn-vue ui library and nuxt color picker module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,12 +13,15 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/fonts',
     'nuxt-echarts',
+    'shadcn-nuxt',
+    'nuxt-color-picker',
   ],
   devtools: { enabled: false },
   app: {
     head: {
       htmlAttrs: {
         class: 'dark',
+        'data-theme-variant': 'blue',
       },
     },
   },
@@ -83,5 +86,8 @@ export default defineNuxtConfig({
     clientBundle: {
       scan: true,
     },
+  },
+  shadcn: {
+    componentDir: './app/components/ui',
   },
 })
