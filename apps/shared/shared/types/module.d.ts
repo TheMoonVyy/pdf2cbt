ed01104a685308ace30194c41ce17a39db2db818
TheMoonVyy <TheMoonVyy@gmail.com>
2025-10-01 16:19:08 +0530

    fix module types declaration

--- apps/shared/shared/types/module.d.ts
+++ apps/shared/shared/types/module.d.ts
@@ -1,3 +1,3 @@
-declare module 'markdown-it-span'
+declare module '#layers/shared/app/assets/external/markdown-it-span'
 declare module 'markdown-it-task-lists'
 declare module 'markdown-it-block-embed'
